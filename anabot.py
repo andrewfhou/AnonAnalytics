b192038191f892ffb2f274180a4a6121e2befea8
Andrew Hou <andrewfhou@gmail.com>
2019-09-14 12:54:15 -0400

    init bot script

--- anabot.py
+++ anabot.py
@@ -1,1 +1,18 @@
 import discord
+
+client = discord.Client()
+token = ""
+
+with open('secret','r') as f:
+    token = f.read()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+client.run(token)
